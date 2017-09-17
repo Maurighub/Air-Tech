d172eaf81ee33db4c7aeb9c1e28b77e3b9612683
Maurighub <joseamarilio2.0@gmail.com>
2017-09-16 21:45:05 -0300

    primeiro commit

---
+++ _js/btn-control.js
@@ -0,0 +1,12 @@
+
+function menu(){
+    var menu=document.getElementById("navigation");
+    var btn=document.getElementById("btn_menu").checked;
+   
+        if(btn==){
+            
+            
+        }else{
+            
+    }
+    
